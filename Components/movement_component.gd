a4f9451a156dc25bf4ca7b76c977d1f42bce0425
JamesBaker101 <jamesambaker@hotmail.com>
2025-04-18 18:27:31 +0930

    i have a big cock and this is my first commit

---
+++ Components/movement_component.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var max_speed: float = 10000
+var acceleration_time: float = 0.1
+@onready var player: CharacterBody2D = get_owner()
+
+func _physics_process(delta: float) -> void:
+	var velocity = player.velocity
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	
+	velocity = velocity.move_toward(input_direction*max_speed, (1.0 / acceleration_time) * delta * max_speed)
+	
+	if input_direction.y && sign(input_direction.y) != sign(velocity.y):
+		velocity.y *= 0.75
+	
+	if input_direction.x && sign(input_direction.x) != sign(velocity.x): 
+		velocity.x *= 0.75
+	
+	player.velocity = velocity
+	player.move_and_slide()
+
+func _ready() -> void:
+	pass
